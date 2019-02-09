9d6ef3a4687eaf75069c43dd61a9a6bac614ee52
Rayan Ahmed <rahmed@ltsp218.udallas.edu>
2019-02-09 13:13:00 -0600

    Fixed errors

--- app/src/main/java/com/example/yeabkalwubshit/booksharing/Book.java
+++ app/src/main/java/com/example/yeabkalwubshit/booksharing/Book.java
@@ -1,26 +1,26 @@
 public class Book {
 
-    private Final String bookname;
-    private Final long id;
+    private final String bookName;
+    private final long id;
 
-    private String owner;
-    private String department;
+    private String ownerName;
+    private String departmentName;
 
     public Book(long id, String bookName) {
         this.id = long;
-        this. bookname = bookName;
+        this.bookName = bookName;
     }
 
-    public void setOwner (String name) {
-        this.owner = name;
+    public void setOwner (String OwnerName) {
+        this.ownerName = ownerName;
     }
-    public void setDepartment (String department) {
-        this.department = department;
+    public void setDepartment (String departmentName) {
+        this.departmentName = departmentName;
     }
 
-    public String getOwner() {return this.owner;}
+    public String getOwnerName() {return this.ownerName;}
     public long getId() {return this.id;}
-    public String getDepartment() {return this.department;}
-    public String getBookName() {return this.name;}
+    public String getDepartment() {return this.departmentName;}
+    public String getBookName() {return this.bookName;}
 
 }
