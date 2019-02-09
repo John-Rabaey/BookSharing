86794b22bf70afd3b41c2ef273a3b20f2afa8838
Rayan Ahmed <rahmed@ltsp18.udallas.edu>
2019-02-09 16:26:30 -0600

    trying to commit again

--- app/src/main/java/com/example/yeabkalwubshit/booksharing/FindABookEntry.java
+++ app/src/main/java/com/example/yeabkalwubshit/booksharing/FindABookEntry.java
@@ -2,6 +2,7 @@ package com.example.yeabkalwubshit.booksharing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class FindABookEntry extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class FindABookEntry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_abook_entry);
+
+        Spinner fieldSpinner = (Spinner)findViewById(R.id.spinnerField);
+        Spinner courseSpinner = (Spinner) findViewById(R.id.spinnerCourse);
+        Spinner professorSpinner = (Spinner) findViewById(R.id.spinnerProfessor);
     }
 }
