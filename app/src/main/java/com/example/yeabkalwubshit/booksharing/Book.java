df207739008f5a9b4708608e39ef2d3c3a2d7c48
Rayan Ahmed <rahmed@ltsp195.udallas.edu>
2019-02-09 12:49:19 -0600

    Basic outline for a book class

---
+++ app/src/main/java/com/example/yeabkalwubshit/booksharing/Book.java
@@ -0,0 +1,26 @@
+public class Book {
+
+    private Final String bookname;
+    private Final long id;
+
+    private String owner;
+    private String department;
+
+    public Book(long id, String bookName) {
+        this.id = long;
+        this. bookname = bookName;
+    }
+
+    public void setOwner (String name) {
+        this.owner = name;
+    }
+    public void setDepartment (String department) {
+        this.department = department;
+    }
+
+    public String getOwner() {return this.owner;}
+    public long getId() {return this.id;}
+    public String getDepartment() {return this.department;}
+    public String getBookName() {return this.name;}
+
+}
