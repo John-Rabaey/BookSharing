c03f8298a599e43da1596d355f07b551d84602e9
Rayan Ahmed <rahmed@ltsp18.udallas.edu>
2019-02-09 16:20:47 -0600

    created a new activity FindABookEntry, waiting on database input to populate spinner with field, course, and professor names

---
+++ app/src/main/java/com/example/yeabkalwubshit/booksharing/FindABookEntry.java
@@ -0,0 +1,13 @@
+package com.example.yeabkalwubshit.booksharing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindABookEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_abook_entry);
+    }
+}
